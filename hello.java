c693d381a5991f06e0b92b8a5fa5a0f15d617774
sushantshekher <112805113+sushantshekher@users.noreply.github.com>
2023-08-19 13:35:15 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,7 +1,9 @@
-class Hello
-{
-Public sta􀆟c void main(String args[]);
-{
-System.out.println(“hello”);
-}
+public class SumCalculator {
+    public static void main(String[] args) {
+        int num1 = 5;
+        int num2 = 7;
+        int sum = num1 + num2;
+
+        System.out.println("Sum of " + num1 + " and " + num2 + " is: " + sum);
+    }
 }
