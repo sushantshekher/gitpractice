0a7d720b9c4c5b40ec63f00ed99f6dfa77a14375
sushantshekher <112805113+sushantshekher@users.noreply.github.com>
2023-08-19 14:45:24 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,7 +1,7 @@
 public class hello {
     public static void main(String[] args) {
         int num1 = 5;
-        int num2 = 7;
+        int num2 = 6;
         int sum = num1 + num2;
 
         System.out.println("Sum of " + num1 + " and " + num2 + " is: " + sum);
